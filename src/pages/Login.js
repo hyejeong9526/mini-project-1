799365cfd31f48b526adff771cebdd32671753af
jinach0i <ginach0irk@gmail.com>
2023-09-22 13:55:49 +0900

    pages폴더 경로 이동

---
+++ src/pages/Login.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [formData, setFormData] = useState({ username: '', password: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" name="username" value={formData.username} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
